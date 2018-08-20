5e8003e10321f194dcd714a5eb46720990bfdc97
thautwarm <twshere@outlook.com>
2018-08-20 14:21:39 +0800

    spec finished

--- LLL/LLL/Core/Machine.fs
+++ LLL/LLL/Core/Machine.fs
@@ -5,9 +5,10 @@ open LLL.ASDL
 type hashmap<'k, 'v> = System.Collections.Generic.Dictionary<'k, 'v>
 
 type context = {
-    prefix      : string
-    ``global``  : hashmap<string, ``type``> // the only external scope in LLL is global scope.
-    bounds      : Map<string, ``type``>
+    prefix                  : string
+    ``global``              : hashmap<string, ``type``> // the only external scope in LLL is global scope.
+    bounds                  : Map<string, ``type``>
+    mutable anonymous_count : int
     }
     with 
     member this.find(name : string) = 
@@ -25,36 +26,89 @@ let mangle (prefix: string) (lexer: string) = sprintf "%s.%s" prefix lexer
 let load (name: string) (``type``: ``type``): llvm = 
     failwith "not impl yet"
 
-let define (context: context) 
-           (name: string) (ret: ``type``) (take: ``type``) (body: lll) =
+let defun (head) (body) : llvm = 
+    failwith "not impl yet"
+
+let call(func) (args) : llvm = 
+    failwith "not impl yet"
+
+let deftype (ty: ``type``) : llvm = 
+    failwith "not impl yet"
+
    
 let rec emit (asdl : lll) (context: context) =
     match asdl with 
+    
     | Lit data  -> 
         failwith "not impl yet"
+    
     | Val {actual_name = actual; lexer_name = lexer}  ->
         // LOAD
         let ``type`` = context.find lexer 
         in load actual ``type``
+
     | Defun {arg_tys = arg_typs
              formals = formals
              ret_ty  = ret_ty
              body    = body
              name    = name} ->
+        
         let ty      =  Func(arg_typs, ret_ty)
         let head    =  load name ty
         context.``global``.[name] <- Func(arg_typs, ret_ty)
         let prefix  = mangle context.prefix name
         let context =
-            List.fold 
-            <| fun context (formal: string) -> 
-               let name = mangle context.prefix formal
-               let value = {actual_name = name; lexer_name = formal}
-               {context with bounds = context.bounds.Add (formal, value)}
-           <| context <| formals
-        let body = emit body context 
-        in 
-            
+            // You see no closure here.
+            List.zip formals arg_typs 
+            |> fun val_lst -> 
+            {context 
+                with bounds = Map.ofList val_lst
+                     prefix = prefix
+                     anonymous_count = 0}
+
+        let body = emit body context
+
+        in defun head body
+    
+    | App(func :: arg_lst) ->
+        let load_val (value : value) = load value.actual_name <| context.find func.lexer_name
+        let func = load_val func 
+        let arg_lst = 
+            List.map load_val arg_lst
+        in call func arg_lst
+    
+
+    | Deftype ``type`` -> deftype(``type``)
+
+    | Lam  {arg_tys = arg_typs
+            formals = formals
+            ret_ty  = ret_ty
+            body    = body} ->
+
+        let name    = sprintf "<anonymous%d>" context.anonymous_count
+        let ty      =  Func(arg_typs, ret_ty)
+        let head    =  load name ty
+        context.``global``.[name] <- Func(arg_typs, ret_ty)
+        let prefix  = mangle context.prefix name
+        let context =
+            // You see no closure here.
+            List.zip formals arg_typs 
+            |> fun val_lst -> 
+            {context 
+                with bounds = Map.ofList val_lst
+                     prefix = prefix
+                     anonymous_count = 0}
+        
+        let body = emit body context
+        in defun head body
+    
+    | Lit data ->
+        failwith "not impl yet"
+
+
+
+          
+        
             
                 
 
