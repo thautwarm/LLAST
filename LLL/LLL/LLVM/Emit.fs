49a83d222271a5c2206c51aadc51e510bbe3df66
thautwarm <twshere@outlook.com>
2018-08-21 17:13:55 +0800

    convert(all kinds of cast)

--- LLL/LLL/LLVM/Emit.fs
+++ LLL/LLL/LLVM/Emit.fs
@@ -1,4 +1,5 @@
-﻿module Emit
+﻿
+module Emit
 
 open IR
 
@@ -70,7 +71,7 @@ let rec typed_data : constant -> (``type`` * string) = function
     | _ as it  -> failwith <| sprintf "unsupported data %A" it
 
 let emit (cr: compiler) =
-    let recur inner(context: context) (llvm: llvm) =
+    let rec inner(context: context) (llvm: llvm): symbol =
         let assign_tmp code =
             let name = context.alloc_name()
             cr.write <| sprintf "%s = %s" name code
@@ -78,14 +79,12 @@ let emit (cr: compiler) =
         match llvm with
         | Const constant ->
             let ty, code = typed_data constant
-            let name = context.alloc_name()
-            assign_tmp name code
-            {ty = ty; name = name}
+            {ty = ty; name = assign_tmp code}
         | Comp(comparator, lhs, rhs) ->
-            let lty, lcode = inner context lhs
-            let rty, rcode = inner context rhs
+            let {ty=lty; name = lname} = inner context lhs
+            let {ty=rty; name = rname} = inner context rhs
             let cmp_cond =
-                match lhs, comparator with
+                match comparator, lty with
                 | Eq, I _ -> "eq"
                 | Eq, F _ -> "oeq"
                 | Ne, I _ -> "ne"
@@ -99,19 +98,80 @@ let emit (cr: compiler) =
                 | Le, I _ -> "sle"
                 | Le, F _ -> "ole"
             match lty, rty with
-            | I bit, I bit ->
+            | I bit, I bit' when bit = bit' ->
                 let type_str = dump_type (I bit)
-                let code = sprintf "icmp %s %s %s, %s" cmp_cond type_str lcode rcode
+                let code = sprintf "icmp %s %s %s, %s" cmp_cond type_str lname rname
                 {ty = I 1; name = assign_tmp code}
-            | F bit, F bit ->
+            | F bit, F bit' when bit = bit' ->
                 let type_str = dump_type (F bit)
-                let code = sprintf "fcmp %s %s %s, %s" cmp_cond type_str lcode rcode
+                let code = sprintf "fcmp %s %s %s, %s" cmp_cond type_str lname rname
                 {ty = I 1; name = assign_tmp code}
-            | Vec(n, elem_ty), Vec(n, elem_ty) ->
-                let type_str = dump_type (I bit)
-                let code = sprintf "icmp %s %s %s, %s" cmp_cond type_str lcode rcode
+            | Vec(n, elem_ty), Vec(n', elem_ty') when n' = n && elem_ty' = elem_ty ->
+                let type_str = dump_type elem_ty
+                let code = sprintf "icmp %s %s %s, %s" cmp_cond type_str lname rname
                 {ty = Arr(n, I 1); name = assign_tmp code}
             | _ -> failwithf "invalid comparison for %A %A" lhs rhs
+        | Conv conversion ->
+            let routine inst sym dest =
+                let {ty=ty; name=name} = sym
+                let code =
+                    sprintf "%s %s %s to %s" inst
+                    <| dump_type ty
+                    <| name
+                    <| dump_type dest
+                {ty = dest; name = assign_tmp code}
+
+            let cond_routine predicate inst src dest =
+                let {ty=ty; name=name} as sym = inner context src
+                if not <| predicate(ty, dest) then failwithf "invalid trunc %A -> %A" src dest
+                else routine inst sym dest
+
+            let is_int = function
+                | I _, I _ -> true
+                | Vec(n, I _), Vec(n', I _) when n = n' -> true
+                | _ -> false
+
+            let is_float = function
+                | F _, F _ -> true
+                | Vec(n, F _), Vec(n', F _) when n = n' -> true
+
+            let is_ptr_int_conv = function
+                | Vec(n, Ptr _), Vec(n', I _) when n = n' -> true
+                | Vec(n, I _), Vec(n', Ptr _) when n = n' -> true
+                | Ptr _, I _
+                | I   _, Ptr _ -> true
+                | _ -> false
+
+            let convert src dest =
+                let {ty=ty} as sym = inner context src
+                let inst =
+                    match ty, dest with
+                    | I _, F _ -> "sitofp"
+                    | Vec(n, I _), Vec(n', F _) when n = n' -> "sitofp"
+                    | F _, I _ -> "fptosi"
+                    | Vec(n, F _), Vec(n', I _) when n = n' -> "fptosi"
+                    | Ptr _, I _ -> "ptrtoint"
+                    | Vec(n, Ptr _), Vec(n', I _) when n = n' -> "ptrtoint"
+                    | I _, Ptr _ -> "inttoptr"
+                    | Vec(n, I _), Vec(n', Ptr _) when n = n' -> "inttoptr"
+                    | a, b when a = b -> ""
+                    | _ -> failwithf "invalid convert %A -> %A" ty dest
+                if inst = "" then sym  // if types of two sides are the same, do nothing
+                else
+                routine inst sym dest
+
+            match conversion with
+            | Truncate(src, dest)   -> cond_routine is_int "trunc" src dest
+            | ZeroExt(src, dest)    -> cond_routine is_int "zext"  src dest
+            | SignExt(src, dest)    -> cond_routine is_int "sext"  src dest
+
+            | FloatTrunc(src, dest) -> cond_routine is_float "fptrunc" src dest
+            | FloatExt(src, dest)   -> cond_routine is_float "fpext" src dest
+
+            | Bitcast(src, dest)    -> cond_routine is_int  "bitcast" src dest
+
+            | Convert(src, dest)    -> convert src dest
         | Sym symbol ->
             symbol
         | _ -> failwith ""
+    in inner
--- LLL/LLL/LLVM/LLVM.fs
+++ LLL/LLL/LLVM/LLVM.fs
@@ -120,5 +120,5 @@ and binary_operation = binary_operator * llvm * llvm
 
 and symbol = {
     ty       : ``type``
-    name     : string option
+    name     : string
 }
