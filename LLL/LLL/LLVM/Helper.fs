9d6b5c53613e8af6a35e71e85fe2d450701258cb
thautwarm <twshere@outlook.com>
2018-08-23 21:43:57 +0800

    infrastructure based on new language definition language finished

--- LLL/LLL/LLVM/Helper.fs
+++ LLL/LLL/LLVM/Helper.fs
@@ -1,13 +1,39 @@
 module LLL.LLVM.Helper
 open LLL.LLVM.ML
+
 type ('k, 'v) hashtable = System.Collections.Generic.Dictionary<'k, 'v>
-let fmt = sprintf
-type context = {
-    ``global`` : (string, ``type``) hashtable
-    local      : (string, ``type``) Map
+
+type symbol = {
+    lexer_name  : string
+    actual_name : string
+    ctx         : context
+    ty          : ``type``
+
+
+}
+
+and context = {
+    ``global`` : (string, symbol) hashtable
+    local      : (string, symbol) Map
     count      : int
     prefix     : string list(** for context mangling usage *)
 }
+
+let void_ctx    = {
+    ``global`` = hashtable()
+    local = Map.ofList []
+    count=0
+    prefix=[]
+}
+let void_symbol = {
+    lexer_name = "<void_val>"
+    actual_name = "<void_name>"
+    ty=Void
+    ctx=void_ctx
+}
+
+let fmt = sprintf
+
 
 type context with
     static member alloc_name ctx =
@@ -19,11 +45,11 @@ type context with
         let name = context.wrap_name ctx name
         let local = ctx.local
         {ctx with local = Map.add name ty local}
-    static member find name ctx : ``type`` =
+    static member find name ctx : symbol  =
         match Map.tryFind name ctx.local with
         | Some ty -> ty
         | None    ->
-        let ref = ref Void
+        let ref = ref void_symbol
         match ctx.``global``.TryGetValue(name, ref) with
         | true  -> ref.Value
         | false ->
@@ -35,78 +61,131 @@ let private concat = String.concat " ,"
 let to_str arg = fmt "%A" arg
 let rec dump_type: ``type`` -> string =
     function
-    | I bit -> fmt "i%d" bit
-    | F 32  -> "float"
-    | F 64  -> "double"
-    | Vec(n, ty) -> fmt "< %d x %s >" <| n <| dump_type ty
-    | Arr(n, ty) -> fmt "[ %d x %s ]" <| n <| dump_type ty
-    | Agg ty_lst -> fmt "{ %s }" <| concat (List.map dump_type ty_lst)
-    | Named name -> fmt "%%struct.%s" name
-    | Ptr ty     -> fmt "%s *" <| dump_type ty
+    | I bit           -> fmt "i%d" bit
+    | F 32            -> "float"
+    | F 64            -> "double"
+    | Vec(n, ty)      -> fmt "< %d x %s >" <| n <| dump_type ty
+    | Arr(n, ty)      -> fmt "[ %d x %s ]" <| n <| dump_type ty
+    | Agg ty_lst      -> fmt "{ %s }" <| concat (List.map dump_type ty_lst)
+    | Alias name      -> fmt "%%struct.%s" name
+    | Ptr ty          -> fmt "%s *" <| dump_type ty
     | Func(args, ret) ->
         fmt "%s (%s)" <| dump_type ret <| concat (List.map dump_type args)
-    | Void       -> "void"
-    | _ as it    -> failwithf "Unsupported type %A." it
+    | Void            -> "void"
+    | _ as it         -> failwithf "Unsupported type %A." it
 
-let inline get_align ty : int =
-    let rec get_align =
+let inline get_align ctx ty : int =
+    let rec get_align (recur_set: string Set) =
         function
+        | PendingTy _ -> failwithf "Type not decided yet."
         | Void   -> 0
         | Func _ -> 0
         | F bit
         | I bit  -> bit/8
         | Ptr _  -> 8
         | Arr(_, ty)
-        | Vec(_, ty)  -> 8 + get_align ty
-        | Agg(ty_lst) -> List.max <| List.map get_align ty_lst
-        | Named name  -> failwithf "get align mustn't accept a named type."
+        | Vec(_, ty)  -> 8 + get_align recur_set ty
+        | Agg(ty_lst) -> List.max <| List.map (get_align recur_set) ty_lst
+        | Alias name when not <| Set.contains name recur_set ->
+            let ty = ctx.``global``.[name].ty
+            get_align <| Set.add name recur_set <| ty
+        | Alias _ -> 0
+        | _  as it    -> failwithf "%A cannot be used to calculate align." it
     in
-    match get_align ty with
+    match get_align <| set [] <| ty with
     | 0 -> 1
     | otherwise -> otherwise
 
-let inline store' ty val_name ptr_name =
+let inline dump_sym {ty = ty; actual_name=actual_name} =
     let ty = dump_type ty
-    fmt "store %s %s, %s %s" ty val_name ptr_name
+    fmt "%s %s" ty actual_name
 
-let inline alloca' ty maybe_val_name =
-    let align = get_align ty
-    let ty    = dump_type ty
-    match maybe_val_name with
-    | None -> fmt "alloca %s, align %d" ty align
-    | Some val_name -> fmt "alloca %s, %s %s, align %d" ty ty val_name align
-let inline load' ty ptr_name =
+let inline store' val' ptr' =
+    fmt "store %s, %s" <| dump_sym val' <| dump_sym ptr'
+
+let inline alloca' ptr' data' =
+    let alloca_ty, is_ptr =
+        match ptr'.ty with
+        | Ptr alloca_ty -> alloca_ty, true
+        | _        -> ptr'.ty, false
+
+    if not is_ptr then
+        failwith "allocation can only be applied on ptr type!"
+    else
+    let align = get_align ptr'.ctx alloca_ty
+    match data' with
+    | None ->
+        fmt "alloca %s, align %d" <| dump_type alloca_ty <| align
+    | Some data' ->
+    fmt "alloca %s, %s ,align %d" <| dump_type alloca_ty <| dump_sym data' <| align
+
+let inline load' {ty=ty; actual_name=ptr_name} =
     let ty = dump_type ty
     fmt "load %s, %s* %s" ty ty ptr_name
 
-let inline gep' ty ptr_name idx offsets =
-    let ty = dump_type ty
-    fmt "getelementptr inbounds %s, %s* %s, %s, %s"
-        ty ty ptr_name idx <| concat offsets
-
-let inline extractelem' vec_ty vec_name idx_ty idx =
-    let vec_ty = dump_type vec_ty
-    let idx_ty = dump_type idx_ty
-    fmt "extractelement %s %s, %s %s" vec_ty vec_name idx_ty idx
-
-let inlne insertelem' vec_ty vec_name val_ty val_name idx_ty idx =
-    let vec_ty = dump_type vec_ty
-    let idx_ty = dump_type idx_ty
+let inline gep' ptr' idx offsets =
+    match ptr'.ty with
+    | Ptr val_ty ->
     let val_ty = dump_type val_ty
-    fmt "insertelement %s %s, %s %s, %s %s" vec_ty vec_name val_ty val_name idx_ty idx
+    fmt "getelementptr inbounds %s, %s, %s, %s"
+        val_ty <| dump_sym ptr' <| idx <| concat offsets
+    | _ -> failwithf "only pointer type could be perform `getelementptr`, got %A." ptr'.ty
 
-let inline extractval' agg_ty agg_name (offsets: int list) =
-    let agg_ty = dump_type agg_ty
-    fmt "extractvalue %s %s, %s" agg_ty agg_name <| concat (List.map to_str offsets)
+let inline extractelem' vec' idx_ty idx =
+    let vec' = dump_sym vec'
+    fmt "extractelement %s, %s %s" vec' idx_ty idx
 
-let inline insertval' agg_ty agg_name elt_ty elt_name offsets =
-    let agg_ty = dump_type agg_ty
-    let elt_ty = dump_type elt_ty
-    fmt "insertvalue %s %s, %s %s, %s"
-        agg_ty agg_name
-        elt_ty elt_name
+let inlne insertelem' vec' val' idx' =
+    fmt "insertelement %s, %s, %s"
+    <| dump_sym vec'
+    <| dump_sym val'
+    <| dump_sym idx'
+
+let inline extractval' agg' (offsets: int list) =
+    fmt "extractvalue %s %s, %s"
+    <| dump_sym agg'
+    <| concat (List.map to_str offsets)
+
+let inline insertval' agg' elt' offsets =
+    fmt "insertvalue %s, %s, %s"
+        <| dump_sym agg'
+        <| dump_sym elt'
         <| concat (List.map to_str offsets)
 
-let typed_data: constant -> (``type`` * string) = function
+let rec typed_data: constant -> (``type`` * string) = function
+    | PendingConst _ -> failwith "Data not decided yet."
     | ID(bit, value) -> I bit, to_str value
-    | 
+    | FD(bit, value) -> F bit, to_str value
+    | ArrD lst       ->
+        let typed_lst, data_lst = List.unzip <| List.map typed_data lst
+        let length = List.length data_lst
+        match List.distinct typed_lst with
+        | [ty] ->
+            Arr(length, ty),
+            fmt "[ %s ]" <| concat data_lst
+        | _    -> failwith "element types mismatch"
+    | VecD lst ->
+        let typed_lst, data_lst = List.unzip <| List.map typed_data lst
+        let length = List.length data_lst
+        match List.distinct typed_lst with
+        | [ty] ->
+            Vec(length, ty),
+            fmt "< %s >" <| concat data_lst
+        | _    -> failwith "element types mismatch"
+    | AggD lst ->
+        let type_lst, data_lst = List.unzip <| List.map typed_data lst
+        let length = List.length data_lst
+        let agg_ty = Agg(type_lst)
+        agg_ty,
+        fmt "{ %s }" <| concat data_lst
+    | Undef ty ->
+        ty, "undef"
+
+
+
+let type_eq =
+    function
+    | Wildcard, _ -> true
+    | _, Wildcard -> true
+    | Alias name1, Alias name2 -> name1 = name2
+    | a, b -> a = b
