e141bf0c0a03d736ba4fe87576c9124abc3fac6a
thautwarm <twshere@outlook.com>
2018-08-25 03:38:40 +0800

    merge language constructs into single datatype

--- LLAST/Entry.fs
+++ LLAST/Entry.fs
@@ -23,7 +23,7 @@ let main args =
 
     let formal_args = [("arg1", I 32)]
     let ret_ty = F 32
-    let body = Conv(CompatCast(Bin(Add, Get "arg1", Get "arg1"), F 32))
+    let body = CompatCast(Bin(Add, Get "arg1", Get "arg1"), F 32)
    
     let defun = Defun("func", formal_args, ret_ty, body)
     test "test type convert" defun
@@ -32,7 +32,7 @@ let main args =
     let ret_ty = F 32
     let body = 
         Let("var", 
-            Conv(CompatCast(Bin(Add, Get "arg1", Get "arg1"), F 32)), 
+            CompatCast(Bin(Add, Get "arg1", Get "arg1"), F 32), 
             Bin(Add, Get("var"), Const(FD(32, 10.0))))
     let def_test1 =  Defun("test1", formal_args, ret_ty, body)
 
@@ -40,17 +40,32 @@ let main args =
     let formal_args = [("arg1", I 32)]
     let ret_ty = I 64
     let body = 
-        Conv(
-            CompatCast(
-                App(Get("test1"), 
-                    [Get("arg1")])
-                    , I 64))
+        CompatCast(
+            App(Get("test1"), 
+                [Get("arg1")])
+                , I 64)
     
     let def_test2 = Defun("test2", formal_args, ret_ty, body)
     
     let suite = Suite([def_test2; def_test1])
     test "cross definition" suite
 
+    let formal_args = [("arg1", I 32)]
+    let ret_ty = I 32
+    let cond = Bin(Eq, Const <| ID(32, 10L), Get("arg1"))
+    let branch = Branch(cond, "truelabel", "falselabel")
+    let alloc = Suite
+    let iftrue = Suite([Mark "truelabel"; Jump "tag"])
+    let iffalse = Suite([Mark "falselabel"; Const <| ID(32, 10L)])
+    let ret = Suite([Mark "tag"; ID(32, 15L) |> Const |> Return])
+    let whole = Defun("test3", formal_args, ret_ty, 
+                      Suite [
+                        branch
+                        iftrue
+                        iffalse
+                        ret
+                       ])
+    test "jump" whole
     0
 
 
--- LLAST/LLVM/Emit.fs
+++ LLAST/LLVM/Emit.fs
@@ -54,7 +54,45 @@ let rec emit (types: type_table) (proc: ref<proc>) =
         let assign_tmp (ctx: context) codestr =
             let name = ctx.alloc_name
             name, Ordered <| fmt "%%%s = %s" name codestr
-    
+
+        let convert_routine inst sym dest =
+                let codestr =
+                   fmt "%s %s to %s"
+                       <| inst
+                       <| dump_sym sym
+                       <| dump_type dest
+                let name, proc' = assign_tmp ctx codestr
+                combine proc'
+                {ty=dest; name=Some name; ty_tb=types; is_glob=false}
+
+        let rec is_int_ext = function
+                | Terminator, I _
+                | I _, Terminator       -> true
+                | I a, I b when a < b -> true
+                | Vec(n, l), Vec(n', r) when n = n' -> is_int_ext(l, r)
+                | _ -> false
+
+        let compat_cast src dest =
+            let {ty=ty} as sym = emit' ctx src
+            let inst =
+                let rec get_inst =
+                    function
+                    | a, b when a =||= b -> ""
+                    | Vec(n, l), Vec(n', r) when n = n' -> get_inst(l, r)
+                    | I _, F _   -> "sitofp"
+                    | F _, I _   -> "fptosi"
+                    | Ptr _, I _ -> "ptrtoint"
+                    | I _, Ptr _ -> "inttoptr"
+                    | I a, I b when a > b -> "trunc"
+                    | I a, I b when a < b -> "sext"
+                    | F a, F b when a > b -> "fptrunc"
+                    | F a, F b when a < b -> "fpext"
+                    | _ -> failwithf "invalid convert %A -> %A" ty dest
+                in get_inst(ty, dest)
+            if inst = "" then sym
+            else
+            convert_routine inst sym dest
+
         function
         | PendingLLVM _ -> failwith "Undecided llvm ast."
         | Const constant ->
@@ -75,7 +113,7 @@ let rec emit (types: type_table) (proc: ref<proc>) =
             let arg_names, arg_tys = List.unzip args
             let func_ty = Func(arg_tys, ret_ty)
             ctx.``global``.[name] <- {ty=func_ty; name=Some name; ty_tb = types; is_glob=true}
-            let delay = fun () -> 
+            let delay = fun () ->
                 let arg_syms =
                     List.map
                     <| fun (arg, ty) -> {ty = ty; name=Some <| ctx.wrap_name arg; ty_tb = types; is_glob=false}
@@ -83,36 +121,30 @@ let rec emit (types: type_table) (proc: ref<proc>) =
 
                 let rec update_map = function
                     | [] -> ()
-                    | (arg_name, arg_sym) :: tail -> 
+                    | (arg_name, arg_sym) :: tail ->
                     ctx.bind arg_name arg_sym
                     update_map tail
                 in update_map <| List.zip arg_names arg_syms
                 let arg_string = List.map dump_sym arg_syms |> join
-        
+
                 let head =
                     fmt "define %s @%s(%s){"
                     <| dump_type ret_ty
                     <| name
                     <| arg_string
                     |> Ordered
-       
                 let inner_proc = ref Empty
                 let body =
-                    emit 
-                    <| types
-                    <| inner_proc
-                    <| ctx
-                    <| body
+                    emit types inner_proc ctx body
 
-                match body with 
+                match body with
                 | {ty=Terminator} -> ()
                 | _ as sym        ->
                     let ret_stmt = fmt "ret %s" <| dump_sym sym
-                    if sym.ty <> ret_ty then 
+                    if sym.ty <> ret_ty then
                         failwithf "function return type %A conflicts with end of body %A" ret_ty sym.ty
                     else
                     inner_proc.contents <- Combine(inner_proc.contents, Ordered ret_stmt)
-            
                 let endl = fmt "}" |> Ordered
                 let defun = Indent(inner_proc.Value)
                 let defun = Combine(defun, endl)
@@ -120,94 +152,58 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 NoIndent <| Predef defun
             combine <| Pending delay
             void_symbol
+        | ZeroExt(src, dest) ->
+            let sym = emit' ctx src
+            if is_int_ext(sym.ty, dest) then convert_routine "zext" sym dest
+            else failwithf "Invalid zero extending at %A" sym.name
 
-        | Conv conversion ->
-            let routine inst sym dest =
-                let codestr =
-                   fmt "%s %s to %s"
-                       <| inst
-                       <| dump_sym sym
-                       <| dump_type dest
-                let name, proc' = assign_tmp ctx codestr
-                combine proc'
-                {ty=dest; name=Some name; ty_tb=types; is_glob=false}
-            
-            let rec is_int_ext = function
-               | Terminator, I _
-               | I _, Terminator       -> true
-               | I a, I b when a < b -> true
-               | Vec(n, l), Vec(n', r) when n = n' -> is_int_ext(l, r)
-               | _ -> false
+        | Bitcast(src, dest) ->
+            let sym = emit' ctx src
+            convert_routine "bitcast" sym dest
 
-            let compat_cast src dest =
-                let {ty=ty} as sym = emit' ctx src
-                let inst =
-                    let rec get_inst =
-                        function
-                        | a, b when a =||= b -> ""
-                        | Vec(n, l), Vec(n', r) when n = n' -> get_inst(l, r)
-                        | I _, F _   -> "sitofp"
-                        | F _, I _   -> "fptosi"
-                        | Ptr _, I _ -> "ptrtoint"
-                        | I _, Ptr _ -> "inttoptr"
-                        | I a, I b when a > b -> "trunc"
-                        | I a, I b when a < b -> "sext"
-                        | F a, F b when a > b -> "fptrunc"
-                        | F a, F b when a < b -> "fpext"
-                        | _ -> failwithf "invalid convert %A -> %A" ty dest
-                    in get_inst(ty, dest)
-                if inst = "" then sym
-                else 
-                routine inst sym dest
-            match conversion with
-            | PendingConv _ -> failwith "Undecided conversion."
-            | ZeroExt(src, dest) ->
-                let sym = emit' ctx src 
-                if is_int_ext(sym.ty, dest) then routine "zext" sym dest
-                else failwithf "Invalid zero extending at %A" sym.name
-            | Bitcast(src, dest) ->
-                let sym = emit' ctx src 
-                routine "bitcast" sym dest
-            | CompatCast(src, dest) ->
-                compat_cast src dest
-        | Cont control_flow ->
-            let increment =
-                match control_flow with 
-                | Return value ->
-                    match emit' ctx value with
-                    | {ty=Void}->
-                        Ordered "ret void"
-                    | _ as sym->
-                        fmt "ret %s" <| dump_sym sym |> Ordered
-                | IndrBr(cond, labels) ->
-                    let cond = emit' ctx cond |> dump_sym
-                    let label_string = List.map 
-                                       <| fun label -> fmt "label %s" label 
-                                       <| labels
-                                       |> join
-                    let codestr = fmt "indirectbr %s, [ %s ]" cond label_string
-                    Ordered codestr
-                | Mark(name) ->
-                    fmt "%s:" name |> Ordered |> NoIndent
-                | Branch(cond, iffalse, iftrue) ->
-                    let cond = emit' ctx cond |> dump_sym
-                    let codestr = fmt "br %s, label %%%s, label %%%s" cond iftrue iffalse
-                    Ordered codestr
-                | Jump(label) ->
-                   Ordered <| fmt "br label %%%s" label
-                | Switch(cond, cases, default') ->
-                    let cond = emit' ctx cond |> dump_sym
-                    let label_pairs = 
-                        List.map 
-                        <| fun (case, label) ->
-                            let case = emit' ctx case |> dump_sym
-                            fmt "%s, label %%%s" case label
-                        <| cases 
-                        |> join
-                    fmt "switch %s, %%%s [ %s ]" cond default' label_pairs |> Ordered
-            combine increment
+        | CompatCast(src, dest) ->
+            compat_cast src dest
+
+        | Return value ->
+            match emit' ctx value with
+            | {ty=Void}->
+                Ordered "ret void"
+            | _ as sym->
+                fmt "ret %s" <| dump_sym sym |> Ordered
+            |> combine
             terminator
-        
+        | IndrBr(cond, labels) ->
+            let cond = emit' ctx cond |> dump_sym
+            let label_string =
+                List.map 
+                <| fun label -> fmt "label %s" label 
+                <| labels
+                |> join
+            let codestr = fmt "indirectbr %s, [ %s ]" cond label_string
+            combine <| Ordered codestr
+            terminator
+        | Mark(name) ->
+            fmt "%s:" name |> Ordered |> NoIndent |> combine
+            terminator
+        | Branch(cond, iffalse, iftrue) ->
+            let cond = emit' ctx cond |> dump_sym
+            let codestr = fmt "br %s, label %%%s, label %%%s" cond iftrue iffalse
+            combine <| Ordered codestr
+            terminator
+        | Jump(label) ->
+            Ordered <| fmt "br label %%%s" label |> combine
+            terminator
+        | Switch(cond, cases, default') ->
+            let cond = emit' ctx cond |> dump_sym
+            let label_pairs = 
+                List.map 
+                <| fun (case, label) ->
+                    let case = emit' ctx case |> dump_sym
+                    fmt "%s, label %%%s" case label
+                <| cases 
+                |> join
+            fmt "switch %s, %%%s [ %s ]" cond default' label_pairs |> Ordered |> combine
+            terminator
         | DefTy(name, ty_lst) ->
             let name = ctx.wrap_name name
             let ty = Agg(ty_lst)
@@ -218,13 +214,11 @@ let rec emit (types: type_table) (proc: ref<proc>) =
         | Bin(bin_op, lhs, rhs) as bin ->
             let {ty=lty; name = lname; is_glob=l_is_glob} = emit' ctx lhs
             let {ty=rty; name = rname; is_glob=r_is_glob} = emit' ctx rhs
-            
             let ty =
                 if lty <> rty then
                     failwithf "type mismatch: %A <> %A" lty rty
                 else
                 lty
-            
             let inst, ret_ty =
                 let rec get_inst_and_ret_ty =
                     function
@@ -271,7 +265,6 @@ let rec emit (types: type_table) (proc: ref<proc>) =
         | App(func, args) ->
             let emit'' = emit' ctx
             let {ty = fn_ty; name = fn_name} = emit'' func
-            
             fn_name |>>
             fun fn_name ->
 
@@ -284,7 +277,6 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 let arg_string = 
                     List.map dump_sym args
                     |> String.concat ", "
-                
                 match fn_ret_ty with
                 | Void ->
                     let codestr = fmt "call void %s(%s)" fn_name arg_string
@@ -297,6 +289,7 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 combine proc'
                 {name=Some name; ty = fn_ret_ty; is_glob=false; ty_tb=types}
             | _ -> failwith "function call type mismatch."
+
         | Suite suite ->
             let rec loop = function
                 | [expr] ->
@@ -306,9 +299,7 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                     loop suite
                 | [] -> void_symbol
             in loop suite
-        | Acc acc ->
-        match acc with
-        | PendingAcc _ -> failwith "Undecided data accessing."
+
         | Alloca(ty, Some(data)) ->
             let data = emit' ctx data
             if ty <> data.ty then
@@ -318,11 +309,13 @@ let rec emit (types: type_table) (proc: ref<proc>) =
             let codestr = alloca' ptr <| Some data
             combine <| Ordered codestr
             ptr
+
         | Alloca(ty, None) ->
             let ptr = {ty=Ptr ty; name = Some <| ctx.alloc_name; ty_tb=types; is_glob=false}
             let codestr = alloca' ptr None
             combine <| Ordered codestr
             ptr
+
         | Load name ->
             let ptr = ctx.find name
             match ptr with
@@ -331,6 +324,7 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 combine proc'
                 {name=Some name; ty_tb=types; is_glob=false; ty=val_ty}
             | _ ->  failwithf "invalid load instruction, expected Ptr of some type, got type %A." ptr.ty
+
         | Store(name, data) ->
             let {ty = ty_of_data} as data = emit' ctx data
             match ctx.find name with
@@ -341,12 +335,13 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 Ordered <| store' data ptr |> combine
                 void_symbol
             | _ as ty -> failwithf "invalid load instruction, expected Ptr of some type, got type %A." ty
+
         | GEP(name, idx, offsets) ->
             let idx = emit' ctx idx
             match ctx.find name with
             | {ty = Ptr ty} as ptr ->
                 let idx = dump_sym idx
-                let ret_ty = 
+                let ret_ty =
                     let rec find_ty offsets agg_ty =
                         match offsets with
                         | [] -> agg_ty
@@ -363,9 +358,5 @@ let rec emit (types: type_table) (proc: ref<proc>) =
                 combine proc'
                 {name=Some name; ty = ret_ty; is_glob=false; ty_tb=types}
             | _ -> failwith "Invalid instruction getelementptr."
-        
         | _ -> raise <| NotImplementedException()
-        
     emit'
-        
-   
