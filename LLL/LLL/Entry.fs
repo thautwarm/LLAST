aede55d3f7b72cb71601b79fbd8b4acebd545907
thautwarm <twshere@outlook.com>
2018-08-24 19:53:23 +0800

    done

--- LLL/LLL/Entry.fs
+++ LLL/LLL/Entry.fs
@@ -11,8 +11,17 @@ let main args =
     let proc = ref Empty
     let emit' = emit <| hashtable() <| proc
     emit' ctx <| Defun("func", formal_args, ret_ty, body) |> ignore
-    printf "%A" proc.Value.to_ir
+    printf "%s" proc.Value.to_ir
 
+
+    let formal_args = [("arg1", I 32)]
+    let ret_ty = F 32
+    let body = Conv(CompatCast(Bin(Add, Get "arg1", Get "arg1"), F 32))
+    let ctx = context.init
+    let proc = ref Empty
+    let emit' = emit <| hashtable() <| proc
+    emit' ctx <| Defun("func", formal_args, ret_ty, body) |> ignore
+    printf "%s" proc.Value.to_ir
     0
 
 
