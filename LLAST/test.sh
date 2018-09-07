da03c3a1020ed1a980bce0067eaf83a1b10bddde
haruhi-s <haruhi.s@protonmail.com>
2018-09-07 23:27:58 +0800

    using llc and gcc instead

--- LLAST/test.sh
+++ LLAST/test.sh
@@ -1,6 +1,8 @@
-for a in ../ir-snippets/*
+for a in ../ir-snippets/*.ll
 do
-    lli $a;
+    llc $a -o $a.s;
+    gcc -C $a.s -o $a.out
+    ./$a.out
     e=$?
     a=$(echo $a| cut -f3 -d/| cut -f1 -d.)
     printf "$e \treturned by $a\n"
