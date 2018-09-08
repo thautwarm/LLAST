7ed102f64fe8714c3180c947dc478fe3f551ebdb
haruhi-s <haruhi.s@protonmail.com>
2018-09-08 22:36:16 +0800

    def type test

--- LLAST/FrontEnd/Lisp.fs
+++ LLAST/FrontEnd/Lisp.fs
@@ -4,5 +4,54 @@ module IR = open LL.IR
 open FastParse.Infras
 open FastParse.Lexer
 open FastParse.Parser
+let lexerTB = 
+    [
+        R "term" "[^\(\)\s]"
+        C "paren" ["("; ")"]
+        R "space" "\s+"
+    ]
+
+let castMap = 
+    Map 
+    <|
+    [
+        "defun", "keyword"
+        "defty", "keyword"
+    ]
+ 
+let between l parser r =
+    let p = both l parser <| fun _ it -> it
+    both p r <| fun it _ -> it 
+
+let defun' = token_by_value_addr "define"
+let defty' = token_by_value_addr "defty"
+
+let term = token_by_name "term"
+
+let l = token_by_value "("
+let r = token_by_value ")"
+
+let id e = e
+
+let rec ty = 
+    let pa = 
+        between l ty_lst r
+        |> trans 
+        <| fun it -> IR.Agg it 
+    let pb = trans term <| fun token ->
+         match token.value with 
+         | "i32" -> IR.I 32
+         | it    -> IR.Alias it
+    either pa pb
+and ty_lst = 
+    rep 
+    <| ty
+    <| 1 
+    <| -1
+    <| id
+
+
+
+    
 
 
--- LLAST/test.sh
+++ LLAST/test.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 for a in ../ir-snippets/*.ll
 do
-    llc $a -o $a.s;
-    gcc -no-pie -C $a.s -o $a.out
-    ./$a.out
-    e=$?
-    a=$(echo $a| cut -f3 -d/| cut -f1 -d.)
-    printf "$e \treturned by $a\n"
+	llc-6.0 $a -o $a.s;
+	gcc -C $a.s -o $a.out;
+	./$a.out;
+	e=$?;
+	a=$(echo $a| cut -f3 -d/ | cut -f1 -d. ) ;
+	printf "$e \treturned by $a\n";
 done
