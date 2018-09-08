517f0351a075358330f42190a6543153fd0f8fd1
haruhi-s <haruhi.s@protonmail.com>
2018-09-08 21:11:17 +0800

    .

--- run_test.py
+++ run_test.py
@@ -6,7 +6,7 @@ os.system('cd LLAST && dotnet.exe run')
 for each in Path("ir-snippets").list_dir(lambda x: x.endswith('.ll')):
     print(f"run <{each}> :")
     os.system(f"llc-6.0 {str(each)!r} -o asm.s")
-    os.system(f"gcc -C asm.s -o out")
+    os.system(f"gcc -no-pie -fPIC asm.s -o out")
     os.system(f"./out ; echo return: $?")
 
     Path("./out").delete()
