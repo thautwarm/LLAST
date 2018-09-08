958abd1a5094cee50364beb9f1bcd6f4a31e27ec
haruhi-s <haruhi.s@protonmail.com>
2018-09-08 21:31:26 +0800

    fixed shell script

--- LLAST/test.sh
+++ LLAST/test.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
 for a in ../ir-snippets/*.ll
 do
-	llc-6.0 $a -o $a.s;
-	gcc -fPIC $a.s -o $a.out;
-	./$a.out;
-	e=$?;
-	a=$(echo $a| cut -f3 -d/ | cut -f1 -d. ) ;
-	printf "$e \treturned by $a\n";
+    llc $a -o $a.s;
+    gcc -no-pie -C $a.s -o $a.out
+    ./$a.out
+    e=$?
+    a=$(echo $a| cut -f3 -d/| cut -f1 -d.)
+    printf "$e \treturned by $a\n"
 done
